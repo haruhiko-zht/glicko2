5a7bf5444b0aff1538e979e3d78d90dba1ef23d0
haruhiko-zht <psp.in.haruhiko@gmail.com>
2021-05-27 13:35:25 +0900

    [add] simple tests

---
+++ tests/Feature/Glicko2Test.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use HaruhikoZHT\Glicko2\Calculator;
+use HaruhikoZHT\Glicko2\Matches;
+use HaruhikoZHT\Glicko2\MatchResult;
+use HaruhikoZHT\Glicko2\Player;
+use PHPUnit\Framework\TestCase;
+
+class Glicko2Test extends TestCase
+{
+    protected Matches $matches;
+    protected Calculator $calculator;
+
+    protected function setUp(): void
+    {
+        $this->matches = new Matches();
+        $this->calculator = new Calculator($this->matches);
+    }
+
+    public function additionProvider(): array
+    {
+        return [
+            [
+                $players = [
+                    'foo' => new Player(rd: 200),
+                    'bar' => new Player(1400, 30, 0.06),
+                    'baz' => new Player(1550, 100, 0.06),
+                    'qux' => new Player(1700, 300, 0.06),
+                ],
+                [
+                    new MatchResult($players['foo'], 80, $players['bar'], 20),
+                    new MatchResult($players['foo'], 60, $players['baz'], 90),
+                    new MatchResult($players['foo'], 30, $players['qux'], 100),
+                ],
+                ['foo' => new Player(1464.06, 151.52, 0.05999)]
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider additionProvider
+     *
+     * @param Player[] $players
+     * @param MatchResult[] $matches
+     * @param Player[] $expected
+     */
+    public function integration(array $players, array $matches, array $expected): void
+    {
+        $this->matches->addPlayers($players);
+        $this->matches->addResults($matches);
+        $this->calculator->updateRating();
+
+        foreach ($expected as $k => $v) {
+            self::assertLessThan(0.1, abs($v->getRating() - $players[$k]->getRating()));
+            self::assertLessThan(0.01, abs($v->getRD() - $players[$k]->getRD()));
+            self::assertLessThan(0.00001, abs($v->getSigma() - $players[$k]->getSigma()));
+        }
+    }
+}
---
+++ tests/Unit/Example.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+class Example extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example(): void
+    {
+        self::markTestIncomplete('This test is not yet implements.');
+    }
+}
